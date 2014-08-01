42d55d3c898887eb990502c294390969b2422ab1
Lucija Sudic <lucija.tikvica@cern.ch>
2014-08-01 15:03:21 +0200

    merged

--- UnfoldingHistogramFactory.C
+++ UnfoldingHistogramFactory.C
@@ -6,16 +6,14 @@
 TH1D * UnfoldingHistogramFactory::createLeadingJetHistogram(std::string s, std::string title) {
   TH1D * h;
 
-  if (true) {
+  if (false) {
     h = new TH1D(s.c_str(),title.c_str(), 10,30., 530.);
 
   } else { // Variable Bin Size
 
     std::vector<double> binLimits;
-
-    double value = 1.;
-    double binSize = 20;
-
+    double value = 30.;
+    double binSize = 40;
     while (value<500.) {
       binLimits.push_back(value);
       if (value>150) binSize = 50.;
@@ -40,10 +38,30 @@ TH1D * UnfoldingHistogramFactory::createLeadingJetHistogram(std::string s, std::
 
 TH1D *  UnfoldingHistogramFactory::createZPtHistogram(std::string s, std::string title) {
 
+  TH1D * h;
 
+  if (false) {
+    h = new TH1D(s.c_str(),title.c_str(), 10,0., 500.);
+  } else {
 
-  TH1D * h = new TH1D(s.c_str(),title.c_str(), 10,0., 500.);
+    std::vector<double> binLimits;
+    double value = 1.;
+    double binSize = 20;
+    while (value<500.) {
+      binLimits.push_back(value);
+      if (value>100) binSize = 25.;
+      if (value>300) binSize = 40.;
+      if (value>400) binSize = 50.;
+      value += binSize;
+    }
+    int nBins = binLimits.size() - 1;
+    double *  bins = new double[nBins+1];
+    for (int i=0; i< binLimits.size(); i++) {
+      bins[i] = binLimits[i];
+    }
+    h = new TH1D(s.c_str(),title.c_str(), nBins, bins);
 
+  }
   return h;
 
 }
