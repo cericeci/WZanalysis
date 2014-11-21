6b202148273600396e46b45f9d57e4aad467a1a8
Lucija Sudic <lucija.tikvica@cern.ch>
2014-11-21 19:40:34 +0100

    systematics added

---
+++ systematics.h
@@ -0,0 +1,12 @@
+#ifndef systematics_h
+#define systematics_h
+
+#define SF_syst 0  //0:no syst, 1:up, -1: down   
+#define pu_syst 0 //same here 
+#define ele_scale_syst 0
+#define mu_scale_syst 0
+#define VVV_xs 0
+#define WV_xs 0
+#define ZZ_xs 0
+#define Zgamma_xs 0
+#endif
